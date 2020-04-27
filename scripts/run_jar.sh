233d50356b649730f654be3a54488a6a52c55c24
meiravm1 <meirav.malka@gmail.com>
2020-04-27 18:29:13 +0300

    dockerfile

--- scripts/run_jar.sh
+++ scripts/run_jar.sh
@@ -1,2 +1,2 @@
 #java -cp "kafkaStreamScala\build\output\lib\*;kafkaStreamScala\build\libs\kafkaStreamScala.jar Covid19Totals
-java -cp "build/output/lib/*;build/libs/kafkaStreamScala.jar" Covid19Totals
+java -cp "/opt/app/*" Covid19Totals
